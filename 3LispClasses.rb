da39b439347af260a3415c6784f0a1505a2963d2
Jun Luo <jun.luo@ageofsignificance.org>
2011-04-26 17:50:55 -0400

    Preliminarily finished factoring the 3Lisp data structure classes and tested the code. Also changed the &&CALL case of 3LispIPP.rb and added the RPP source code.

---
+++ 3LispClasses.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+require './3LispRubyBasics.rb'
+require './3LispPair.rb'
+require './3LispRail.rb'
+require './3LispClosure.rb'
+require './3LispHandle.rb'
+require './3LispEnvironment.rb'
